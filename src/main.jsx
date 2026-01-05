1c08c06612429fd068175e5447d8ad21bb1b7475
Pietro Munoz <fenice.ecuador@gmail.com>
2026-01-04 22:32:30 -0500

    fix: lo-que-sea

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // 🔥 IMPORT CRÍTICO – NO BORRAR – NO MOVER
-import "./i18n"; // o "./i18n/i18n.js" según tu path real
+import "./i18n/i18n"; // o "./i18n/i18n.js" según tu path real
 
 import App from "./App.jsx";
 
