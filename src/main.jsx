2cf2e30be8dbe84961290294eca503d7f1c878e5
Pietro Munoz <fenice.ecuador@gmail.com>
2026-02-24 20:48:03 -0500

    fix(i18n): wrap app with I18nextProvider to remove init warning

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
 
-import "./i18n/i18n";
+import i18n from "./i18n/i18n";
 import "./index.css";
 
 import { AuthProvider } from "@/auth/AuthProvider.jsx";
@@ -13,10 +14,12 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <I18nextProvider i18n={i18n}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </I18nextProvider>
   </React.StrictMode>
 );
