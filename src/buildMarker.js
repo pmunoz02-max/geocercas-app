9408a36a7b43bd0ff8418a94dc19f136700f5448
Pietro Munoz <fenice.ecuador@gmail.com>
2026-03-27 16:06:44 -0500

    chore: restore build marker

--- src/buildMarker.js
+++ src/buildMarker.js
@@ -2,6 +2,5 @@
 export const BUILD_MARKER = "preview-7db5d01";
 
 if (typeof window !== "undefined") {
-  // visible en consola para confirmar deploy real
   console.log("[BUILD_MARKER]", BUILD_MARKER);
 }
