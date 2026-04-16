e10e5333047d0af8e19039d9af160ca9d73a683f
Pietro Munoz <fenice.ecuador@gmail.com>
2026-04-16 10:29:29 -0500

    i18n: use initialized shared instance in app provider

--- src/main.jsx
+++ src/main.jsx
@@ -28,8 +28,7 @@ class ErrorBoundary extends React.Component {
 import "./buildMarker.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import i18n from "i18next";
-import "./i18n/i18n";
+import i18n from "./i18n/i18n";
 
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
