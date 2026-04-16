9362b7d2a2a17ae1594f51c12e1ab235ce0b7819
Pietro Munoz <fenice.ecuador@gmail.com>
2026-04-16 09:26:35 -0500

    i18n: unify shared instance across modules

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,23 +1,2 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-
-import { es } from "./locales/es";
-import { en } from "./locales/en";
-import { fr } from "./locales/fr";
-
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      es: { translation: es },
-      en: { translation: en },
-      fr: { translation: fr },
-    },
-    lng: "es", // 👈 DEFAULT FORZADO
-    fallbackLng: "es",
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-
+import i18n from "./i18n";
 export default i18n;
