2c3673557f14ee36a693c31fab15b71276bee6fd
Pietro Munoz <fenice.ecuador@gmail.com>
2026-05-01 21:37:35 -0500

    fix french activities field labels

--- scripts/fix-fr-activities-labels.cjs
+++ scripts/fix-fr-activities-labels.cjs
@@ -9,24 +9,37 @@ data.actividades = data.actividades || {};
 Object.assign(data.actividades, {
   title: "Activités",
   subtitle: "Catalogue des activités avec coût horaire.",
+
   fieldName: "Nom",
+  fieldNameLabel: "Nom",
   fieldNamePlaceholder: "Ex. Semis, Irrigation, Récolte",
+
   fieldHourlyRateLabel: "Coût horaire",
+
   fieldCurrency: "Devise",
-  fieldDescription: "Description",
+  fieldCurrencyLabel: "Devise",
+
+  fieldDescription: "Description (facultative)",
+  fieldDescriptionLabel: "Description (facultative)",
   fieldDescriptionOptional: "Description (facultative)",
   fieldDescriptionPlaceholder: "Description courte de l’activité...",
+
   buttonCreate: "Créer une activité",
   buttonSave: "Enregistrer les modifications",
+
   actionEdit: "Modifier",
   actionActivate: "Activer",
   actionDeactivate: "Désactiver",
   actionDelete: "Supprimer",
+
   statusActive: "Active",
   statusInactive: "Inactive",
-  perHour: "par heure"
+  perHour: "par heure",
+
+  errorRateInvalid: "Saisissez un coût horaire valide.",
+  errorRatePositive: "Le coût horaire doit être supérieur à 0."
 });
 
 fs.writeFileSync(file, JSON.stringify(data, null, 2) + "\n", "utf8");
 JSON.parse(fs.readFileSync(file, "utf8"));
-console.log("fr activities labels fixed");
+console.log("fr.json actividades fixed");
